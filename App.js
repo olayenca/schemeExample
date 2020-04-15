dcdefc867e8970d1571fd68dd0e0c0452de6ab3e
olayenca <olayinkaotuniyi@yahoo.co.uk>
2020-04-15 21:14:46 +0100

    Instagram-oAuth fixed requestInstagram

--- App.js
+++ App.js
@@ -17,61 +17,61 @@ import {
   StatusBar,
 } from 'react-native';
 import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
-import requestInstagram from './requestInstagram';
-
+import requestInstagram, {instagramTokens} from './requestInstagram';
 
 const App: () => React$Node = () => {
   const [yourUserName, setUserName] = useState('');
   const [yourId, setid] = useState('');
 
-  const onClick = () => {
-    const webpage = 'https://olayenca.github.io/iosCallbacks';
-    const url = `'https://api.instagram.com/oauth/authorize?client_id=1075008359508721&redirect_uri=${webpage}&scope=user_profile,user_media&response_type=code`;
+  const onClick = async () => {
+    const {client_id, redirect_uri} = instagramTokens;
+    const apiUrl = `https://api.instagram.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=user_profile,user_media&response_type=code`;
     const scheme = 'appName://';
 
-   const result =  requestInstagram(url, scheme);
-   const {username, id} = result;  console.log({result});
-   setUserName(username)
-   setid(id);
+    const result = await requestInstagram(apiUrl, scheme);
+    const {username, id} = result;
 
+    setUserName(username);
+    setid(id);
   };
 
-  const content = userName.length ? (
-      <Text style={styles.sectionDescription}>
-        {' '}
-        Click this link to login in to approve app
-      </Text>
+  let content = !yourUserName?.length ? (
+    <Text style={styles.sectionDescription}>
+      {' '}
+      Click this link to login in to approve app
+    </Text>
   ) : (
-      <Text style={styles.sectionDescription}>
-        {yourUserName}: {yourId}
-      </Text>
+    <Text style={styles.sectionDescription}>
+      {yourUserName}: {yourId}
+    </Text>
   );
+
   return (
-      <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <ScrollView
-              contentInsetAdjustmentBehavior="automatic"
-              style={styles.scrollView}>
-            <Header />
-            {global.HermesInternal == null ? null : (
-                <View style={styles.engine}>
-                  <Text style={styles.footer}>Engine: Hermes</Text>
-                </View>
-            )}
-            <View style={styles.body}>
-              <View style={styles.sectionContainer}>
-                <Button
-                    title="Login into Instagram"
-                    onPress={onClick}
-                    style={styles.sectionTitle}
-                />
-                {content}
-              </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
-          </ScrollView>
-        </SafeAreaView>
-      </>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Button
+                title="Login into Instagram"
+                onPress={onClick}
+                style={styles.sectionTitle}
+              />
+              {content}
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
